b4ce8148aea0df14bc31cd4eae3f2a3b27e15bee
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-05-24 16:53:25 +0200

    Restauration view ended

--- src/Entity/Restaurant.php
+++ src/Entity/Restaurant.php
@@ -31,6 +31,9 @@ class Restaurant
     #[ORM\Column(type: 'string', length: 255)]
     private $Type;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private $image;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -111,6 +114,18 @@ class Restaurant
     public function setType(string $Type): self
     {
         $this->Type = $Type;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
--- src/Form/RestaurantType.php
+++ src/Form/RestaurantType.php
@@ -6,18 +6,59 @@ use App\Entity\Restaurant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\HttpFoundation\File\File;
+
 
 class RestaurantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Name')
-            ->add('City')
-            ->add('Street')
-            ->add('AddressNumber')
-            ->add('Description')
-            ->add('Type')
+            ->add('Name', TextType::class, [
+                'label' => 'Nazwa',
+                'attr' => ['class' => 'widget__field'],
+                'label_attr' => ['class' => 'label__field'],
+                'row_attr' => ['class' => 'login__field__in__form'],
+            ])
+            ->add('City',TextType::class, [
+                'label' => 'Miasto',
+                'attr' => ['class' => 'widget__field'],
+                'label_attr' => ['class' => 'label__field'],
+                'row_attr' => ['class' => 'login__field__in__form'],
+            ])
+            ->add('Street',TextType::class, [
+                'label' => 'Ulica',
+                'attr' => ['class' => 'widget__field'],
+                'label_attr' => ['class' => 'label__field'],
+                'row_attr' => ['class' => 'login__field__in__form'],
+            ])
+            ->add('AddressNumber',TextType::class, [
+                'label' => 'Numer budynku',
+                'attr' => ['class' => 'widget__field'],
+                'label_attr' => ['class' => 'label__field'],
+                'row_attr' => ['class' => 'login__field__in__form'],
+            ])
+            ->add('Description',TextType::class, [
+                'label' => 'Opis',
+                'attr' => ['class' => 'widget__field'],
+                'label_attr' => ['class' => 'label__field'],
+                'row_attr' => ['class' => 'login__field__in__form'],
+            ])
+            ->add('Type',TextType::class, [
+                'label' => 'Typ',
+                'attr' => ['class' => 'widget__field'],
+                'label_attr' => ['class' => 'label__field'],
+                'row_attr' => ['class' => 'login__field__in__form'],
+            ])
+            ->add('Image',TextType::class, [
+                'label' => 'Zdjęcie',
+                'attr' => ['class' => 'img__field'],
+                'label_attr' => ['class' => 'label__field'],
+                'row_attr' => ['class' => 'login__field__in__form'],
+            ])
         ;
     }
 
