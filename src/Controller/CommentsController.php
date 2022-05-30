01ce208a688ff0af59f42423c6f5545a919c54c9
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-05-30 10:55:38 +0200

    Comment controller path fix

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -17,14 +17,6 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/opinie')]
 class CommentsController extends AbstractController
 {
-    #[Route('/', name: 'app_comments_index', methods: ['GET'])]
-    public function index(CommentsRepository $commentsRepository): Response
-    {
-        return $this->render('comments/index.html.twig', [
-            'comments' => $commentsRepository->findAll(),
-        ]);
-    }
-
     #[Route('/dodaj', name: 'app_comments_new', methods: ['GET', 'POST'])]
     public function new(Request $request, CommentsRepository $commentsRepository, RestaurantRepository $Restaurant): Response
     {
@@ -43,22 +35,16 @@ class CommentsController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $commentsRepository->add($comment, true);
 
-            return $this->redirectToRoute('app_comments_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_restaurant_show', array('id' => $value,
+            ));
         }
 
-        return $this->renderForm('comments/new.html.twig', [
+        return $this->renderForm('comments/new.html.twig',[
             'comment' => $comment,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_comments_show', methods: ['GET'])]
-    public function show(Comments $comment): Response
-    {
-        return $this->render('comments/show.html.twig', [
-            'comment' => $comment,
-        ]);
-    }
 
     #[Route('/{id}/edycja', name: 'app_comments_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Comments $comment, CommentsRepository $commentsRepository): Response
@@ -69,7 +55,7 @@ class CommentsController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $commentsRepository->add($comment, true);
 
-            return $this->redirectToRoute('app_comments_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_user_panel', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('comments/edit.html.twig', [
@@ -85,6 +71,6 @@ class CommentsController extends AbstractController
             $commentsRepository->remove($comment, true);
         }
 
-        return $this->redirectToRoute('app_comments_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_user_panel', [], Response::HTTP_SEE_OTHER);
     }
 }
