29b6e40ccaaed868dbd501fa4e026c72b1a609df
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-06-06 13:46:19 +0200

    Deployment fixes 5

--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
+<?php
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
@@ -5,3 +7,4 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+?>
