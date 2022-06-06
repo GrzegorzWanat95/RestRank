81327172c9ccef31bb02f4dc2fb3ad5e8f1d0b1f
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-06-06 11:03:42 +0200

    deploument fix part 3

--- migrations/Version20220519072824.php
+++ migrations/Version20220519072824.php
@@ -10,6 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
+/*
 final class Version20220519072824 extends AbstractMigration
 {
     public function getDescription(): string
@@ -30,4 +31,4 @@ final class Version20220519072824 extends AbstractMigration
         $this->addSql('ALTER TABLE opinions CHANGE text text VARCHAR(255) DEFAULT \'NULL\'');
         $this->addSql('ALTER TABLE restaurant CHANGE Nazwa name VARCHAR(50) NOT NULL');
     }
-}
+}*/
