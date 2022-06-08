e711ae9908c019b9c940bf5f2c73a62f04c1cdcc
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-06-08 12:27:26 +0200

    Deployment fixes 6

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -89,13 +89,12 @@ class RestaurantController extends AbstractController
     #[Route('/szukaj/{Type}/{Name}', name: 'app_restaurant_query_name', methods: ['GET', 'POST'])]
     public function searchByName(RestaurantRepository $restaurantRepository, Request $request, Restaurant $restaurant)
     {
-        $form = $this->createForm(RestaurantType::class, $restaurant);
-        $form->handleRequest($request);
-        #$value = dump($request->query->get('Type'));
-        $name = dump($request->query->get('Name'));
+
+        $name = dump(($request)->query->get('Name'));
+        $vname = $name->match('/foo')['_route'];
         return $this->render('restaurant/index.html.twig', [
             'restaurants' => $restaurantRepository ->findBy(
-                ['Name' => $name]
+                ['Name' => $vname]
             )
         ]);
         /*switch ($value){
