fd03133f361c1d1582b161b90bfca931a38c5bca
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-06-08 11:10:09 +0200

    Deployment fixes 6

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -86,8 +86,8 @@ class RestaurantController extends AbstractController
     }
 
 
-    #[Route('/szukaj/{Type}/{Name}', name: 'app_restaurant_query_name', methods: ['GET', 'POST'])]
-    public function searchByName(RestaurantRepository $restaurantRepository, Request $request): Response
+    #[Route('/szukaj/{Type}/{Name}', name: 'app_restaurant_query_name', methods: ['GET'])]
+    public function searchByName(RestaurantRepository $restaurantRepository, Request $request)
     {
         $value = dump($request->query->get('Type'));
         $name = dump($request->query->get('Name'));
